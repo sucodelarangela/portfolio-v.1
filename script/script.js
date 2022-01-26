44c62948ba0e33c577dbb12894981020a6c86a3b
Angela Caldas <aes.caldas@gmail.com>
2022-01-26 13:33:36 -0300

    Create mobile menu

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+//Effects on hover
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
@@ -12,3 +13,22 @@ function changeHeaderOnScroll() {
 window.addEventListener('scroll', () => {
   changeHeaderOnScroll()
 })
+
+//Toggle menu open
+const nav = document.querySelector('nav')
+const menuButton = document.querySelectorAll('.toggle')
+
+for (const elem of menuButton) {
+  elem.addEventListener('click', () => {
+    nav.classList.toggle('show')
+  })
+}
+
+//Close menu when selecting
+const menu = document.querySelectorAll('nav ul li a')
+
+for (const item of menu) {
+  item.addEventListener('click', () => {
+    nav.classList.remove('show')
+  })
+}
